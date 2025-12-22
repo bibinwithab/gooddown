0ac89dfe82f806fd256e93a3bae299828fa9bba3
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2025-12-22 09:10:47 +0530

    prod ready

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,5 +7,10 @@ export default defineConfig({
     react(),
     tailwindcss(), // Add the plugin to the plugins array
   ],
+  build: {
+    outDir: '../backend/public', // Adjust the output directory
+    emptyOutDir: true,
+  },
+  base: '/', // Set the base URL for the project
   // For v4, PostCSS config is often no longer needed in many setups
 });
